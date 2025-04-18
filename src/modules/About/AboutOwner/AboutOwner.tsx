9a646d4eb5b7ef309433c5578b120d64b0ce488d
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-18 16:07:28 +0300

    updated styles for desk

--- src/modules/About/AboutOwner/AboutOwner.tsx
+++ src/modules/About/AboutOwner/AboutOwner.tsx
@@ -12,14 +12,14 @@ const AboutOwner = ({
 }) => {
   return (
     <div className="bg-orange-bg">
-      <div className="grid grid-cols-1 gap-6 xl:grid-cols-2 xl:gap-0">
+      <div className="grid grid-cols-1 gap-6 xl:grid-cols-2 xl:gap-[200px] laptop:gap-0">
         <InfoBlock
           titleClassName="xl:text-left"
           className="px-[30px] py-[40px] flex flex-col justify-center rounded-[20px] xl:px-[89px] xl:py-10"
           translation={translation}
         />
         <motion.div
-          className="relative aspect-[722/689]  mx-auto xl:mx-0 xl:max-h-[680px] xl:w-full"
+          className="relative w-full max-w-[532px] xl:max-w-[732px] laptop:max-w-full aspect-[722/689] xl:aspect-[543/830] laptop:aspect-[543/640] desk:aspect-[722/689] mx-auto xl:mx-0 rounded-[20px] overflow-hidden"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true }}
@@ -30,7 +30,7 @@ const AboutOwner = ({
             src="/images/owner_2.jpg"
             alt="Foundation Owner"
             fill
-            className="object-contain object-top rounded-[20px] xl:object-cover"
+            className="object-top object-cover"
             quality={75}
             priority
           />
