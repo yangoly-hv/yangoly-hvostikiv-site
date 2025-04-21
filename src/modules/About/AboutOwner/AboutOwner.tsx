fab610af1bbd5370d594d0cd8a8bda0a5bb6e325
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-21 13:24:58 +0300

    updated about owner section

--- src/modules/About/AboutOwner/AboutOwner.tsx
+++ src/modules/About/AboutOwner/AboutOwner.tsx
@@ -1,10 +1,9 @@
+"use client";
 import InfoBlock from "@/shared/components/InfoBlock/InfoBlock";
 import { IAboutOwnerTranslation } from "@/shared/types";
 import Image from "next/image";
-import * as motion from "motion/react-client";
-
-import AnimatedWrapper from "@/shared/components/Animations/AnimationWrapper";
-import { fadeInAnimation } from "@/shared/components/Animations/animationVariants";
+import { motion } from "framer-motion";
+import { generalSlideUp } from "@/shared/utils";
 
 const AboutOwner = ({
   translation,
@@ -12,42 +11,28 @@ const AboutOwner = ({
   translation: IAboutOwnerTranslation;
 }) => {
   return (
-    <div className="grid grid-cols-1 gap-6 xl:grid-cols-2 xl:gap-[200px] laptop:gap-0">
+    <div className="grid grid-cols-1 gap-6 xl:grid-cols-2 xl:gap-0">
       <InfoBlock
-        titleClassName="xl:text-left"
-        className="px-[30px] py-[40px] flex flex-col justify-center rounded-[20px] xl:px-[89px] xl:py-10"
+        titleClassName="laptop:w-[583px] xl:text-left"
+        className="px-[30px] py-[40px] flex flex-col justify-center rounded-[20px] xl:px-[75px] laptop:px-[89px] xl:py-10"
         translation={translation}
       />
-
       <motion.div
         className="relative w-full max-w-[532px] xl:max-w-[732px] laptop:max-w-full aspect-[722/689] xl:aspect-[543/830] laptop:aspect-[543/640] desk:aspect-[722/689] mx-auto xl:mx-0 rounded-[20px] overflow-hidden"
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true }}
-        variants={fadeInAnimation({ y: 30 })}
+        variants={generalSlideUp}
+        custom={0.4}
       >
-        {/* <Image
+        <Image
           src="/images/owner_2.jpg"
           alt="Foundation Owner"
           fill
           className="object-top object-cover"
           quality={75}
           priority
-        /> */}
-        <AnimatedWrapper
-          as="div"
-          animation={fadeInAnimation({ scale: 0.9, delay: 0.2 })}
-          className="relative aspect-[722/689]  mx-auto xl:mx-0 xl:max-h-[680px] xl:w-full"
-        >
-          <Image
-            src="/images/owner_2.jpg"
-            alt="Foundation Owner"
-            fill
-            className="object-contain object-top rounded-[20px] xl:object-cover"
-            quality={75}
-            priority
-          />
-        </AnimatedWrapper>
+        />
       </motion.div>
     </div>
   );
