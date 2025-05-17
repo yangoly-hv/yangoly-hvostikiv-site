6e98b3f61e579c8a8ec373e323633948e0c66c38
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-17 15:13:46 +0300

    updated blog card styles

--- src/modules/Blog/BlogCard.tsx
+++ src/modules/Blog/BlogCard.tsx
@@ -19,63 +19,66 @@ export default function BlogCard({
     <div
       className={`flex flex-col justify-between h-full max-w-[343px] desk:max-w-[436px] pt-8 pb-5 px-6 bg-white rounded-[20px] shadow-blogCard ${className}`}
     >
-      {mainPhoto && (
-        <motion.div
-          variants={slideUp}
+      <div>
+        {" "}
+        {mainPhoto && (
+          <motion.div
+            variants={slideUp}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            custom={0}
+          >
+            <Link href={`/blog/${slug}`} className="block mb-[26px]">
+              <div className="relative w-full h-[246px] desk:h-[323px] aspect-[295/246] rounded-[11.25px] overflow-hidden">
+                <Image
+                  src={mainPhoto}
+                  alt={title}
+                  fill
+                  className="object-cover object-center rounded-[11.25px]"
+                  sizes="(max-width: 1280px) 50vw, 33vw"
+                />
+              </div>
+            </Link>
+          </motion.div>
+        )}
+        <motion.p
+          variants={fadeIn}
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true }}
-          custom={0}
+          custom={0.2}
+          className="mb-3 text-dark text-[16px] font-medium leading-[20.8px]"
         >
-          <Link href={`/blog/${slug}`} className="block mb-[26px]">
-            <div className="relative w-full h-[246px] desk:h-[323px] aspect-[295/246] rounded-[11.25px]">
-              <Image
-                src={mainPhoto}
-                alt={title}
-                fill
-                className="object-cover object-center rounded-[11.25px]"
-                sizes="(max-width: 1280px) 50vw, 33vw"
-              />
-            </div>
+          {date}
+        </motion.p>
+        <motion.div
+          variants={fadeIn}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          custom={0.4}
+        >
+          <Link href={`/blog/${slug}`}>
+            <h2
+              className="min-h-[52px] mb-3 text-dark text-[20px] font-semibold leading-[26px] line-clamp-2 focus-visible:text-primary-gray xl:hover:text-primary-gray
+            transition duration-300 ease-out"
+            >
+              {title}
+            </h2>
           </Link>
         </motion.div>
-      )}
-      <motion.p
-        variants={fadeIn}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true }}
-        custom={0.2}
-        className="mb-3 text-dark text-[16px] font-medium leading-[20.8px]"
-      >
-        {date}
-      </motion.p>
-      <motion.div
-        variants={fadeIn}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true }}
-        custom={0.4}
-      >
-        <Link href={`/blog/${slug}`}>
-          <h2
-            className="mb-3 text-dark text-[20px] font-semibold leading-[26px] line-clamp-2 focus-visible:text-primary-gray xl:hover:text-primary-gray
-            transition duration-300 ease-out"
-          >
-            {title}
-          </h2>
-        </Link>
-      </motion.div>
-      <motion.p
-        variants={fadeIn}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true }}
-        custom={0.6}
-        className="mb-5 text-dark font-normal text-[14px] leading-[18.2px] line-clamp-4"
-      >
-        <PortableTextRenderer value={description} />
-      </motion.p>
+        <motion.p
+          variants={fadeIn}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          custom={0.6}
+          className="mb-5 text-dark font-normal text-[14px] leading-[18.2px] line-clamp-4"
+        >
+          <PortableTextRenderer value={description} />
+        </motion.p>
+      </div>
       <motion.div
         variants={fadeIn}
         initial="hidden"
