60f4c9ecd85a757e994c21cb5dac9bbb74cd4354
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-08-25 12:32:12 +0300

    updated rendom cards

--- src/shared/components/RandomTailCards/RandomTailCards.tsx
+++ src/shared/components/RandomTailCards/RandomTailCards.tsx
@@ -6,8 +6,8 @@ import { IRandomTailCardsProps } from "@/shared/types";
 const RandomTailCards = ({ tails, translation }: IRandomTailCardsProps) => {
   const itemsToShow = useRandomCardsPerPage();
   return (
-    <div className="flex justify-center">
-      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 laptop:grid-cols-4 gap-5 mt-6 pb-[120px]">
+    <div className="w-full">
+      <ul className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 laptop:grid-cols-4 gap-5 mt-6 pb-[120px]">
         {tails.slice(0, itemsToShow).map((randomTail, index) => (
           <TailCard key={index} tail={randomTail} translation={translation} />
         ))}
